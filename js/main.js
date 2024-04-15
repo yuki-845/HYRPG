ada8b9d2d767043f42011fa8709c31ced9e28500
yicode <120433484+yuki-845@users.noreply.github.com>
2024-04-15 15:26:23 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
-var canvas = document.createElement('canvas');
-const deviceScreenWidth = screen.width;
-const deviceScreenHeight = screen.height
-canvas.width = deviceScreenWidth;
-canvas.height = deviceScreenHeight;
-document.body.appendChild(canvas);
-var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('myCanvas');
+canvas.width = window.screen.width;
+canvas.height = window.screen.height
 
-// Canvas上に描画
-ctx.fillStyle = 'rgb(200, 0, 0)'; // 赤色
-ctx.fillRect(10, 10, 50, 50); // 四角形を描画
+
+var ctx = canvas.getContext('2d');
+        
+var isDrawing = false; // クリックしている間はtrueに設定されます。
+
+
+// マウスクリックイベントをリッスン
+canvas.addEventListener('mousedown', function(event) {
+    isDrawing = true;
+});
+
+// マウスがCanvas上から離れたら、クリックしていないとして処理します。
+canvas.addEventListener('mouseup', function(event) {
+    isDrawing = false;
+});
+
+// マウスがCanvas上を移動するときにイベントをリッスンします。
+canvas.addEventListener('mousemove', function(event) {
+    if (isDrawing) { // クリックされている場合のみ座標を表示します。
+        var rect = canvas.getBoundingClientRect(); // Canvas要素の位置とサイズを取得
+        var x = event.clientX - rect.left; // マウスの現在位置のx座標を計算
+        var y = event.clientY - rect.top; // マウスの現在位置のy座標を計算
+        console.log('Mouse moved to (' + x + ', ' + y + ')');
+    }
+});
+
+ctx.rect(10, 10, 100, 100);
+ctx.fill();
